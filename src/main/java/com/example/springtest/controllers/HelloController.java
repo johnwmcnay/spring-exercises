afc1b2a45bd2cef41aa6eca266996f4f5a17b99f
John McNay <john.w.mcnay@gmail.com>
2020-11-24 15:54:48 -0600

    created DiceController

---
+++ src/main/java/com/example/springtest/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.springtest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
